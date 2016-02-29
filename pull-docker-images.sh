a382f545b6ca39086c493066d97e17b9895852b5
Pawel Szafran <pawel.szafran@gmail.com>
2016-02-28 19:56:50 -0500

    Replace curl with httpie

--- pull-docker-images.sh
+++ pull-docker-images.sh
@@ -2,5 +2,5 @@ docker pull gcr.io/google_containers/etcd:2.2.1
 docker pull gcr.io/google_containers/hyperkube:v1.1.7
 docker pull gcr.io/google_containers/kube2sky:1.12
 docker pull gcr.io/google_containers/skydns:2015-10-13-8c72f8c
-docker pull radial/busyboxplus:curl
 docker pull golang:1.6-onbuild
+docker pull clue/httpie
